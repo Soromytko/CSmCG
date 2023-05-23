871c43d6586ab602a72066c61e848c2c2200072c
Soromytko <NikitaSoromytko@gmail.com>
2023-05-23 17:53:43 +0300

    add laba5

---
+++ Laba5/src/lamp.js
@@ -0,0 +1,82 @@
+var lampVertexShaderSourceCode = `
+precision mediump float;
+
+attribute vec3 a_VertexPosition;
+
+uniform mat4 u_ProjectMat;
+uniform mat4 u_ViewMat;
+uniform mat4 u_WorldMat;
+
+void main()
+{
+   gl_Position = u_ProjectMat * u_ViewMat * u_WorldMat * vec4(a_VertexPosition, 1.0);
+}
+`
+
+var lampFragmentShaderSourceCode = `
+precision mediump float;
+
+uniform vec3 u_Color;
+
+void main()
+{
+    gl_FragColor = vec4(u_Color, 1.0);
+}
+`
+
+
+var vs1 = `
+precision mediump float;
+
+attribute vec3 a_VertexPosition;
+
+uniform mat4 u_ProjectMat;
+uniform mat4 u_ViewMat;
+uniform mat4 u_WorldMat;
+
+void main()
+{
+   gl_Position = u_ProjectMat * u_ViewMat * u_WorldMat * vec4(a_VertexPosition, 1.0);
+}
+`
+
+var fs1 = `
+precision mediump float;
+
+uniform vec3 u_Color;
+
+void main()
+{
+    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+}
+`
+
+class LampMaterial extends Material{
+    constructor() {
+        super()
+
+        if (this._instance) {
+            return this._instance
+        }
+
+        this._vertexShaderSourceCode = lampVertexShaderSourceCode
+        this._fragmentShaderSourceCode = lampFragmentShaderSourceCode
+
+        this._instance = this
+    }
+
+    _init() {
+        const shaderProgram = this._shaderProgram
+        // Uniforms
+        this._uProjectMatLoc = gl.getUniformLocation(shaderProgram, 'u_ProjectMat')
+        this._uViewMatLoc = gl.getUniformLocation(shaderProgram, 'u_ViewMat')
+        this._uWorldMatLoc = gl.getUniformLocation(shaderProgram, 'u_WorldMat')
+        this._uColorLoc = gl.getUniformLocation(shaderProgram, "u_Color")
+
+        // Attributes
+        this._aVertexPositionLoc = gl.getAttribLocation(shaderProgram, 'a_VertexPosition')
+        this._aVertexNormalLoc = gl.getAttribLocation(shaderProgram, 'a_VertexNormal')
+
+        // gl.enableVertexAttribArray(this._aVertexPositionLoc)
+    }
+}
