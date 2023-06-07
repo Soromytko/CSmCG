8605118880160d6f9cb9f0d92c8263814d9f1434
Soromytko <NikitaSoromytko@gmail.com>
2023-06-07 13:24:00 +0300

    separate input

--- Laba5/src/index.js
+++ Laba5/src/index.js
@@ -1,6 +1,3 @@
-// const lambertShader = new Shader(vertexShaderSourceCode, fragmentShaderSourceCode)
-// const phongShader = new Shader(vertPhongSource, fragPhongSource)
-
 const SHADERS = {
     simple: new Shader(simpleVertSrc, simpleFragSrc),
     lambert: new Shader(lambertVertSrc, lambertFragSrc),
@@ -47,10 +44,6 @@ let greenCube
 let yellowCube
 let blueCube
 
-// lightCube,
-
-// debugCube,
-
 function buildShaders() {
     for (const shader in SHADERS) {
         if (!SHADERS[shader].build()) {
@@ -81,7 +74,6 @@ function createScene() {
     yellowCube = createCube({x: 0.5, y: 0, z: 0}, 0.5, {r: 1, g: 1, b: 0})
     blueCube = createCube({x: 0, y: 0.5, z: 0}, 0.5, {r: 0, g: 0, b: 1})
     lightCube = createCube({x: 0, y: 1, z: -1}, 0.1, {r: 1, g: 1, b: 1})
-    // debugCube = new CubeObject({x: 0, y: 0, z: 0}, 0.3, {r: 1, g: 0, b: 1})
     
     pedestal.parent = scene
     redCube.parent = pedestal
@@ -89,134 +81,6 @@ function createScene() {
     yellowCube.parent = pedestal
     blueCube.parent = pedestal
     lightCube.parent = scene
-
-
-    // Create materials
-    // redCube.meshRenderer.material.shader = SHADERS.test2
-    // greenCube.meshRenderer.material.shader = SHADERS.test3
-    // yellowCube.meshRenderer.material.shader = SHADERS.test3
-    // blueCube.meshRenderer.material.shader = SHADERS.test3
-    // lightCube.meshRenderer.material.shader = SHADERS.lamp
-}
-
-function bindInput() {
-    const engine = new Engine()
-    engine.addEventListener("keyup", (e) => {
-        // console.log("AAAAAQ")
-    })
-
-    window.onkeydown = function(e) {
-        const key = e.key.toUpperCase()
-        switch(key) {
-            case "A": {
-                cameraTarget.pos.x -= 0.1
-                return
-            }
-            case "D": {
-                cameraTarget.pos.x += 0.1
-                return
-            }
-            case "Q": {
-                cameraTarget.pos.y -= 0.1
-                return
-            }
-            case "E": {
-                cameraTarget.pos.y += 0.1
-                return
-            }
-            case "S": {
-                cameraTarget.pos.z += 0.1
-                return
-            }
-            case "W": {
-                cameraTarget.pos.z -= 0.1
-                return
-            }
-        }        
-    }
-    
-}
-
-function bindGUI() {
-    document.getElementById("sceneRange").addEventListener("input", (event) => {
-        scene.rotation.y = event.target.value / 60
-    })
-
-    document.getElementById("pedestalRange").addEventListener("input", (event) => {
-        pedestal.rotation.y = event.target.value / 60
-    })
-
-    document.getElementById("redRange").addEventListener("input", (event) => {
-        redCube.rotation.y = event.target.value / 50
-    })
-
-    document.getElementById("greenRange").addEventListener("input", (event) => {
-        greenCube.rotation.y = event.target.value / 50
-    })
-
-    document.getElementById("yellowRange").addEventListener("input", (event) => {
-        yellowCube.rotation.y = event.target.value / 50
-    })
-
-    document.getElementById("blueRange").addEventListener("input", (event) => {
-        blueCube.rotation.y = event.target.value / 50
-    })
-
-    document.getElementById("ambientRange").addEventListener("input", (event) => {
-        ambientIntensity = event.target.value
-    })
-
-    document.getElementById("diffuseRange").addEventListener("input", (event) => {
-        diffuseIntensity = event.target.value * 10
-    })
-
-    document.getElementById("specularRange").addEventListener("input", (event) => {
-        specularIntensity = event.target.value
-    })
-
-
-    document.getElementById("lightSizeRange").addEventListener("input", (event) => {
-        lightSize = event.target.value * 10
-    })
-
-    document.getElementById("lightXRange").addEventListener("input", (event) => {
-        lightCube.position.x = event.target.value * 10 - 5 // [-5, 5]
-    })
-
-    document.getElementById("lightYRange").addEventListener("input", (event) => {
-        lightCube.position.y = event.target.value * 10 - 5 // [-5, 5]
-    })
-
-    document.getElementById("lightZRange").addEventListener("input", (event) => {
-        lightCube.position.z = event.target.value * 10 - 5 // [-5, 5]
-    })
-
-    document.getElementById("lightingModelSelect").addEventListener("change", (event) => {
-        const setShader = function(shader) {
-            redCube.meshRenderer.material.shader = shader
-            greenCube.meshRenderer.material.shader = shader
-            yellowCube.meshRenderer.material.shader = shader
-            blueCube.meshRenderer.material.shader = shader
-        }
-
-        const model = event.target.value
-        if (model == "Simple") {
-            setShader(SHADERS.simple)
-            console.log("Simple material is set")
-        } else if (model == "Lambert") {
-            setShader(SHADERS.lambert)
-            console.log("Lambert material is set")
-        } else if (model == "Phong") {
-            setShader(SHADERS.phong)
-            console.log("Phong material is set")
-        } else if (model == "Guro") {
-            setShader(SHADERS.guro)
-            console.log("Guro material is set")
-        }
-        else {
-            console.log("Unknown model: ", model)
-        }
-    })
 }
 
 function length(vector) {
@@ -225,7 +89,6 @@ function length(vector) {
 
 function normalize(vector) {
     const l = length(vector)
-    // const result = {x: vector.x / l, y: vector.y / l, z: vector.z / l}
     return {x: vector.x / l, y: vector.y / l, z: vector.z / l}
 }
 
---
+++ Laba5/src/input.js
@@ -0,0 +1,114 @@
+
+function bindInput() {
+    window.onkeydown = function(e) {
+        const key = e.key.toUpperCase()
+        switch(key) {
+            case "A": {
+                cameraTarget.pos.x -= 0.1
+                return
+            }
+            case "D": {
+                cameraTarget.pos.x += 0.1
+                return
+            }
+            case "Q": {
+                cameraTarget.pos.y -= 0.1
+                return
+            }
+            case "E": {
+                cameraTarget.pos.y += 0.1
+                return
+            }
+            case "S": {
+                cameraTarget.pos.z += 0.1
+                return
+            }
+            case "W": {
+                cameraTarget.pos.z -= 0.1
+                return
+            }
+        }        
+    }
+}
+
+function bindGUI() {
+    document.getElementById("sceneRange").addEventListener("input", (event) => {
+        scene.rotation.y = event.target.value / 60
+    })
+
+    document.getElementById("pedestalRange").addEventListener("input", (event) => {
+        pedestal.rotation.y = event.target.value / 60
+    })
+
+    document.getElementById("redRange").addEventListener("input", (event) => {
+        redCube.rotation.y = event.target.value / 50
+    })
+
+    document.getElementById("greenRange").addEventListener("input", (event) => {
+        greenCube.rotation.y = event.target.value / 50
+    })
+
+    document.getElementById("yellowRange").addEventListener("input", (event) => {
+        yellowCube.rotation.y = event.target.value / 50
+    })
+
+    document.getElementById("blueRange").addEventListener("input", (event) => {
+        blueCube.rotation.y = event.target.value / 50
+    })
+
+    document.getElementById("ambientRange").addEventListener("input", (event) => {
+        ambientIntensity = event.target.value
+    })
+
+    document.getElementById("diffuseRange").addEventListener("input", (event) => {
+        diffuseIntensity = event.target.value * 10
+    })
+
+    document.getElementById("specularRange").addEventListener("input", (event) => {
+        specularIntensity = event.target.value
+    })
+
+
+    document.getElementById("lightSizeRange").addEventListener("input", (event) => {
+        lightSize = event.target.value * 10
+    })
+
+    document.getElementById("lightXRange").addEventListener("input", (event) => {
+        lightCube.position.x = event.target.value * 10 - 5 // [-5, 5]
+    })
+
+    document.getElementById("lightYRange").addEventListener("input", (event) => {
+        lightCube.position.y = event.target.value * 10 - 5 // [-5, 5]
+    })
+
+    document.getElementById("lightZRange").addEventListener("input", (event) => {
+        lightCube.position.z = event.target.value * 10 - 5 // [-5, 5]
+    })
+
+    document.getElementById("lightingModelSelect").addEventListener("change", (event) => {
+        const setShader = function(shader) {
+            redCube.meshRenderer.material.shader = shader
+            greenCube.meshRenderer.material.shader = shader
+            yellowCube.meshRenderer.material.shader = shader
+            blueCube.meshRenderer.material.shader = shader
+        }
+
+        const model = event.target.value
+        if (model == "Simple") {
+            setShader(SHADERS.simple)
+            console.log("Simple material is set")
+        } else if (model == "Lambert") {
+            setShader(SHADERS.lambert)
+            console.log("Lambert material is set")
+        } else if (model == "Phong") {
+            setShader(SHADERS.phong)
+            console.log("Phong material is set")
+        } else if (model == "Guro") {
+            setShader(SHADERS.guro)
+            console.log("Guro material is set")
+        }
+        else {
+            console.log("Unknown model: ", model)
+        }
+    })
+}
